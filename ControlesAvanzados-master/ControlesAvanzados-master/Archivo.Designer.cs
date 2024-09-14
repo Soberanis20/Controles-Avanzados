14a2b79327fbc3390a72f8bf9d9d978344e88485
Soberanis20 <javier.soberanis26@gmail.com>
2024-09-13 21:48:14 -0600

    Add files via upload

---
+++ ControlesAvanzados-master/ControlesAvanzados-master/Archivo.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace ControlesAvanzados
+{
+    partial class Archivo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listadoVentas = new FlowLayoutPanel();
+            selectorDepartamento = new ListBox();
+            comboBoxAnios = new ComboBox();
+            comboBoxMeses = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(10, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(275, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Formulario de Nuevo Archivo";
+            // 
+            // listadoVentas
+            // 
+            listadoVentas.AutoScroll = true;
+            listadoVentas.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            listadoVentas.Location = new Point(267, 49);
+            listadoVentas.Margin = new Padding(3, 2, 3, 2);
+            listadoVentas.Name = "listadoVentas";
+            listadoVentas.Size = new Size(554, 465);
+            listadoVentas.TabIndex = 1;
+            listadoVentas.ControlAdded += panelAdd;
+            // 
+            // selectorDepartamento
+            // 
+            selectorDepartamento.FormattingEnabled = true;
+            selectorDepartamento.ItemHeight = 15;
+            selectorDepartamento.Location = new Point(28, 207);
+            selectorDepartamento.Margin = new Padding(3, 2, 3, 2);
+            selectorDepartamento.Name = "selectorDepartamento";
+            selectorDepartamento.SelectionMode = SelectionMode.MultiSimple;
+            selectorDepartamento.Size = new Size(217, 124);
+            selectorDepartamento.TabIndex = 3;
+            selectorDepartamento.SelectedValueChanged += selectorDepartamento_SelectedValueChanged;
+            // 
+            // comboBoxAnios
+            // 
+            comboBoxAnios.FormattingEnabled = true;
+            comboBoxAnios.Location = new Point(28, 75);
+            comboBoxAnios.Margin = new Padding(3, 2, 3, 2);
+            comboBoxAnios.Name = "comboBoxAnios";
+            comboBoxAnios.Size = new Size(216, 23);
+            comboBoxAnios.TabIndex = 4;
+            // 
+            // comboBoxMeses
+            // 
+            comboBoxMeses.FormattingEnabled = true;
+            comboBoxMeses.Location = new Point(31, 141);
+            comboBoxMeses.Margin = new Padding(3, 2, 3, 2);
+            comboBoxMeses.Name = "comboBoxMeses";
+            comboBoxMeses.Size = new Size(214, 23);
+            comboBoxMeses.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(29, 181);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Seleccionar Depatamento";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 54);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Seleccionar Año";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(32, 117);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Seleccionar Mes";
+            // 
+            // Archivo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(830, 525);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBoxMeses);
+            Controls.Add(comboBoxAnios);
+            Controls.Add(selectorDepartamento);
+            Controls.Add(listadoVentas);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Archivo";
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "Archivo";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private FlowLayoutPanel listadoVentas;
+        private ListBox selectorDepartamento;
+        private ComboBox comboBoxAnios;
+        private ComboBox comboBoxMeses;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ ControlesAvanzados-master/ControlesAvanzados-master/Archivo.cs
@@ -0,0 +1,189 @@
+﻿using ControlesAvanzados.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlesAvanzados
+{
+    public partial class Archivo : Form
+    {
+        List<Venta> ventas = new List<Venta>();
+        public Archivo()
+        {
+            InitializeComponent();
+            agregarVentas();
+            mostrarVentas();
+            inicializarListBox();
+            inicializarComboBoxAnios();
+            inicializarComboBoxMeses();
+
+            comboBoxAnios.SelectedIndexChanged += comboBoxAnios_SelectedIndexChanged;
+            comboBoxMeses.SelectedIndexChanged += comboBoxMeses_SelectedIndexChanged;
+
+        }
+
+        private void agregarVentas()
+        {
+            
+            ventas.Add(new Venta(2024, 1, "Guatemala", 100000));
+            ventas.Add(new Venta(2024, 2, "Guatemala", 80000));
+            ventas.Add(new Venta(2024, 3, "Guatemala", 95000));
+            ventas.Add(new Venta(2024, 4, "Guatemala", 120000));
+            ventas.Add(new Venta(2024, 5, "Guatemala", 100000));
+            ventas.Add(new Venta(2024, 6, "Guatemala", 110000));
+            ventas.Add(new Venta(2024, 1, "Jutiapa", 50000));
+            ventas.Add(new Venta(2024, 2, "Jutiapa", 80000));
+            ventas.Add(new Venta(2024, 3, "Jutiapa", 67000));
+            ventas.Add(new Venta(2024, 4, "Jutiapa", 55000));
+            ventas.Add(new Venta(2024, 5, "Jutiapa", 67000));
+            ventas.Add(new Venta(2024, 6, "Jutiapa", 45000));
+            ventas.Add(new Venta(2024, 1, "Chiquimula", 43000));
+            ventas.Add(new Venta(2024, 2, "Chiquimula", 55000));
+            ventas.Add(new Venta(2024, 3, "Chiquimula", 23000));
+            ventas.Add(new Venta(2024, 4, "Chiquimula", 34000));
+            ventas.Add(new Venta(2024, 5, "Chiquimula", 56000));
+            ventas.Add(new Venta(2024, 6, "Chiquimula", 78000));
+            ventas.Add(new Venta(2024, 1, "Escuintla", 86000));
+            ventas.Add(new Venta(2024, 2, "Escuintla", 75000));
+            ventas.Add(new Venta(2024, 3, "Escuintla", 64000));
+            ventas.Add(new Venta(2024, 4, "Escuintla", 78000));
+            ventas.Add(new Venta(2024, 5, "Escuintla", 79000));
+            ventas.Add(new Venta(2024, 6, "Escuintla", 90000));
+            ventas.Add(new Venta(2024, 6, "Zacapa", 10000));
+        }
+
+        private void mostrarVentas()
+        {
+            listadoVentas.Controls.Clear();
+            
+            string departamentoSeleccionado = selectorDepartamento.SelectedItems.Count > 0 ? selectorDepartamento.SelectedItem.ToString() : null;
+            int anioSeleccionado = comboBoxAnios.SelectedItem != null && comboBoxAnios.SelectedItem.ToString() != "Todos" ? (int)comboBoxAnios.SelectedItem : 0;
+            string mesSeleccionado = comboBoxMeses.SelectedItem != null && comboBoxMeses.SelectedItem.ToString() != "Todos" ? comboBoxMeses.SelectedItem.ToString()
+        : null;
+
+            foreach (Venta venta in ventas)
+            {
+                string nombreMesVenta = obtenerNombreMesPorNumero(venta.Mes);
+
+                if ((departamentoSeleccionado == null || departamentoSeleccionado == venta.Departamento) &&
+                    (anioSeleccionado == 0 || anioSeleccionado == venta.Anio) &&
+                    (mesSeleccionado == null || mesSeleccionado == nombreMesVenta))
+                {
+                    Label labelVenta = crearEqituetaVenta(venta);
+                    listadoVentas.Controls.Add(labelVenta);
+                    
+                }
+            }
+
+        }
+
+        private void inicializarListBox()
+        {
+            List<string> departamentos = new List<string>();
+            foreach (Venta venta in ventas)
+            {
+                if (!departamentos.Contains(venta.Departamento))
+                {
+                    departamentos.Add(venta.Departamento);
+                }
+            }
+            foreach (string departamento in departamentos)
+            {
+                selectorDepartamento.Items.Add(departamento);
+            }
+        }
+
+        private void inicializarComboBoxAnios()
+        {
+            comboBoxAnios.Items.Clear();
+
+            comboBoxAnios.Items.Add("Todos");
+            List<int> anios = new List<int>();
+            foreach (Venta venta in ventas)
+            {
+                if (!anios.Contains(venta.Anio))
+                {
+                    anios.Add(venta.Anio);
+                }
+            }
+            foreach (int anio in anios)
+            {
+                comboBoxAnios.Items.Add(anio);
+            }
+
+            comboBoxAnios.SelectedIndex = 0;
+        }
+
+        private void inicializarComboBoxMeses()
+        {
+            comboBoxMeses.Items.Clear();
+
+            comboBoxMeses.Items.Add("Todos");
+
+            List<string> meses = new List<string>();
+            foreach (Venta venta in ventas)
+            {
+                string nombreMes = obtenerNombreMesPorNumero(venta.Mes);
+                if (!meses.Contains(nombreMes))
+                {
+                    meses.Add(nombreMes);
+                }
+            }
+            foreach (string mes in meses)
+            {
+                comboBoxMeses.Items.Add(mes);
+            }
+
+            comboBoxMeses.SelectedIndex = 0;
+        }
+
+        private string obtenerNombreMesPorNumero(int numeroMes)
+        {
+            string[] meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+            return meses[numeroMes - 1];
+        }
+
+        private Label crearEqituetaVenta(Venta venta)
+        {
+            Label labelVenta = new Label();
+            labelVenta.Text = $"Año: {venta.Anio} \n Mes: {obtenerNombreMesPorNumero(venta.Mes)} \n Departamento: {venta.Departamento} \n Ventas: Q.{(venta.Ventas).ToString("N", new CultureInfo("es-GT"))}";
+            labelVenta.AutoSize = true;
+            labelVenta.Font = new Font("Arial", 10, FontStyle.Bold);
+            labelVenta.Padding = new Padding(10);
+            labelVenta.BorderStyle = BorderStyle.FixedSingle;
+            labelVenta.Margin = new Padding(10);
+            labelVenta.BackColor = Color.LightGray;
+            labelVenta.AutoSize = false;
+            labelVenta.Width = 247;
+            labelVenta.Height = 95;
+            return labelVenta;
+        }
+
+        private void selectorDepartamento_SelectedValueChanged(object sender, EventArgs e)
+        {
+            mostrarVentas();
+
+        }
+        private void comboBoxAnios_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mostrarVentas();
+        }
+
+        private void comboBoxMeses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mostrarVentas();
+        }
+
+        private void panelAdd(object sender, ControlEventArgs e)
+        {
+            
+        }
+    }
+}
